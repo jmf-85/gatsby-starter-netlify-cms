269fa37661f166de93918ac3f0f374be853e69c7
jmf-85 <67808595+jmf-85@users.noreply.github.com>
2020-07-10 09:16:19 +1000

    Add files via upload

---
+++ gatsby-plugin-percy/gatsby-node.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const { execSync } = require('child_process')
+const which = require('which')
+const { isArray } = Array
+
+exports.onPostBuild = ({ store }, { files, ignore, config }) => {
+  let { program: { useYarn, directory } } = store.getState()
+
+  let manager = which.sync(useYarn ? 'yarn' : 'npx')
+  let cmd = [manager, 'percy', 'snapshot', path.resolve(directory, 'public')]
+
+  if (files) {
+    cmd = cmd.concat('--snapshot-files', [].concat(files).join(','))
+  }
+
+  if (ignore) {
+    cmd = cmd.concat('--ignore-files', [].concat(ignore).join(','))
+  }
+
+  if (config) {
+    cmd = cmd.concat('--config', config)
+  }
+
+  execSync(cmd.join(' '), { stdio: 'inherit' })
+}
